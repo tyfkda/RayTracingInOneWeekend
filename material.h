d91833613f43004369f5290fe0fa12b7db5bfa14
tyfkda <tyfkda@gmail.com>
2020-01-05 12:56:09 +0900

    10. Dielectrics

--- material.h
+++ material.h
@@ -7,6 +7,19 @@ inline vec3 reflect(const vec3& v, const vec3& n) {
   return v - 2 * dot(v, n) * n;
 }
 
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+  auto cos_theta = dot(-uv, n);
+  vec3 r_out_parallel = etai_over_etat * (uv + cos_theta * n);
+  vec3 r_out_perp = -sqrt(1.0 - r_out_parallel.length_squared()) * n;
+  return r_out_parallel + r_out_perp;
+}
+
+inline double schlick(double cosine, double ref_idx) {
+  double r0 = (1 - ref_idx) / (1 + ref_idx);
+  r0 = r0 * r0;
+  return r0 + (1 - r0) * pow(1 - cosine, 5);
+}
+
 class material {
  public:
   virtual bool scatter(
@@ -47,4 +60,44 @@ class metal : public material {
  public:
   vec3 albedo;
   double fuzz;
+};
+
+class dielectric : public material {
+ public:
+  dielectric(double ri) : ref_idx(ri) {}
+
+  virtual bool scatter(
+      const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+  ) const override {
+    attenuation = vec3(1.0, 1.0, 1.0);
+    double etai_over_etat;
+    if (rec.front_face) {
+      etai_over_etat = 1.0 / ref_idx;
+    } else {
+      etai_over_etat = ref_idx;
+    }
+
+    vec3 unit_direction = unit_vector(r_in.direction());
+    double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+    if (etai_over_etat * sin_theta > 1.0 ) {
+      vec3 reflected = reflect(unit_direction, rec.normal);
+      scattered = ray(rec.p, reflected);
+      return true;
+    }
+
+    double reflect_prob = schlick(cos_theta, etai_over_etat);
+    if (random_double() < reflect_prob) {
+      vec3 reflected = reflect(unit_direction, rec.normal);
+      scattered = ray(rec.p, reflected);
+      return true;
+    }
+
+    vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+    scattered = ray(rec.p, refracted);
+    return true;
+  }
+
+ public:
+  double ref_idx;
 };
