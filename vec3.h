3f8f413d8f11564d15662c1932c9f2f1857a7b78
tyfkda <tyfkda@gmail.com>
2020-01-05 11:51:26 +0900

    8. Diffuse Materials

--- vec3.h
+++ vec3.h
@@ -8,6 +8,14 @@ class vec3 {
   vec3() : e{0, 0, 0} {}
   vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
+  inline static vec3 random() {
+    return vec3(random_double(), random_double(), random_double());
+  }
+
+  inline static vec3 random(double min, double max) {
+    return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+  }
+
   double x() const { return e[0]; }
   double y() const { return e[1]; }
   double z() const { return e[2]; }
@@ -94,4 +102,27 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
   return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+  while (true) {
+    auto p = vec3::random(-1,1);
+    if (p.length_squared() >= 1) continue;
+    return p;
+  }
+}
+
+vec3 random_unit_vector() {
+  auto a = random_double(0, 2 * pi);
+  auto z = random_double(-1, 1);
+  auto r = sqrt(1 - z * z);
+  return vec3(r * cos(a), r * sin(a), z);
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+  vec3 in_unit_sphere = random_in_unit_sphere();
+  if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+    return in_unit_sphere;
+  else
+    return -in_unit_sphere;
 }
